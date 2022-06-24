1b571a1642b9aa8c71ba19a41721bc7394237665
yusong <yusong@pop-os.localdomain>
2022-06-25 02:16:02 +0800

    update

--- config.fish
+++ config.fish
@@ -19,11 +19,11 @@ function gpip
 end
 
 # pyenv
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-status --is-interactive; and pyenv init --path | source
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+# status --is-interactive; and pyenv init --path | source
+# status --is-interactive; and pyenv init - | source
+# status --is-interactive; and pyenv virtualenv-init - | source
 
 set -x PATH "$HOME/.gitbin" $PATH
 
@@ -34,32 +34,9 @@ set -x PATH "$HOME/.go/current/bin" $PATH
 
 # rust
 set -x PATH "$HOME/.cargo/bin" $PATH
-
-# haskell
-set -x PATH "$HOME/.cabal/bin" $PATH
-
-# OS specific setting
-switch (uname)
-    case Darwin
-        source (dirname (status --current-filename))/config-osx.fish
-    case Linux
-        source (dirname (status --current-filename))/config-linux.fish
-    case '*'
-        source (dirname (status --current-filename))/config-windows.fish
-end
-
-# 3rd party shell tools
-# direnv hook fish | source
-navi widget fish | source
-source /usr/local/opt/asdf/libexec/asdf.fish
-
 # user configs
 source "$HOME/.config/fish/aliases.fish"
 source "$HOME/.config/fish/utils.fish"
 source "$HOME/.config/fish/peco.fish"
-source "$HOME/.config/fish/pyenv.fish"
+#source "$HOME/.config/fish/pyenv.fish"
 
-# Created by `pipx` on 2022-01-17 13:21:07
-set PATH $PATH /Users/steven/.local/bin
-
-export PATH="$PATH:/Users/steven/.foundry/bin"
